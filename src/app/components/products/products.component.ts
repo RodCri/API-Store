401cfbd1c29645efe9bd8a2d561b471152ef4a31
RodCri <cristian.rodriguez1317@gmail.com>
2022-03-03 17:47:36 -0500

    Request Get Post Put Delete and Url params pagination

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product } from '../../models/product.model';
+import { Product, CreateProductDTO, UpdateProductDTO } from '../../models/product.model';
 
 import { StoreService } from '../../services/store.service';
 import { ProductsService } from '../../services/products.service';
@@ -15,7 +15,24 @@ export class ProductsComponent implements OnInit {
   myShoppingCart: Product[] = [];
   total = 0;
   products: Product[] = [];
+  showProductDetail = false;
 
+  productChosen: Product = {
+    id: '',
+    price: 0,
+    images: [],
+    title: '',
+    category: {
+      id: '',
+      name: ''
+    },
+    description: ''
+  };
+
+  limit = 10;
+  offset = 0;
+
+  // Inyectamos los servicios creados en el constructor
   constructor(
     private storeService: StoreService,
     private productsService: ProductsService
@@ -24,7 +41,8 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.productsService.getAllProducts()
+    this.loadMore();
+    this.productsService.getProductsByPage(10,0)
     .subscribe(data => {
       this.products = data;
     });
@@ -35,4 +53,62 @@ export class ProductsComponent implements OnInit {
     this.total = this.storeService.getTotal();
   }
 
+  toggleProductDetail(){
+    this.showProductDetail = !this.showProductDetail;
+  }
+
+  onShowDetail(id: string){
+    this.productsService.getProduct(id)
+    .subscribe(data =>{
+      this.toggleProductDetail();
+      this.productChosen = data;
+    })
+  }
+  
+  createProduct(){
+    const productNew: CreateProductDTO = {
+      title: "new Product",
+      description: "new description",
+      images: ['https://placeimg.com/640/480/any?random=${Math.random()}'],
+      categoryId: 2,
+      price: 1000
+    }
+    this.productsService.create(productNew)
+    .subscribe(data =>{
+      console.log(data);
+      this.products.unshift(data);
+    });
+  }
+
+  updateProduct(){
+    const changes: UpdateProductDTO = {
+      title: "nuevo title"
+    }
+    const id = this.productChosen.id;
+    this.productsService.update(id, changes)
+    .subscribe(data =>{
+      const productIndex = this.products.findIndex(item => item.id=== this.productChosen.id);
+      this.products[productIndex] = data;
+      this.productChosen = data;
+    });
+  }
+
+  deleteProduct(){
+    const id = this.productChosen.id;
+    this.productsService.delete(id)
+    .subscribe(()=>{
+      const productIndex = this.products.findIndex(item => item.id=== this.productChosen.id);
+      this.products.splice(productIndex, 1);
+      this.showProductDetail = false;
+    })
+  }
+
+  loadMore(){
+    this.productsService.getProductsByPage(this.limit,this.offset)
+    .subscribe(data => {
+      // this.products.concat(data);
+      this.products.push(...data)
+      this.offset += this.limit;
+    });
+  }
 }
--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,9 +1,22 @@
+export interface Category {
+  id: string;
+  name: string;
+}
+
+
 // Creamos la interface para productos para que de esta manera tengamos tipado nuestro proyecto
 export interface Product {
   id: string;
   title: string;
   price: number;
-  image: string;
+  images: string[];
   description: string;
-  category: string;
+  category: Category;
 }
+
+export interface CreateProductDTO extends Omit<Product, 'id' | 'category'>{
+  categoryId: number;
+}
+
+// Para campos opcionales
+export interface UpdateProductDTO extends Partial<CreateProductDTO>  {}
